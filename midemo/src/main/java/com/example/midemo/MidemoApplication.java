0b29ef863eda88ba2f59913e1b9234efc7d7a99d
dejavis <misdatosdeseguridadkey@gmail.com>
2026-04-23 16:18:08 -0600

    Proyecto creado en Spring e inicializado

---
+++ midemo/src/main/java/com/example/midemo/MidemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.midemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidemoApplication.class, args);
+	}
+
+}
---
+++ midemo/src/test/java/com/example/midemo/MidemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.midemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
